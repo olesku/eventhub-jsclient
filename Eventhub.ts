94d9e271a9ea87cb8e26d62dc05b4459a3ba9109
Ole Fredrik Skudsvik <ole.skudsvik@gmail.com>
2019-10-31 22:39:06 +0100

    Implementation work

--- Eventhub.ts
+++ Eventhub.ts
@@ -1,41 +1,145 @@
-declare type SubscriptionCallback = (err: object, message: object) => void;
+import WebSocket from 'ws';
+
+export declare type SubscriptionCallback = (err: object, message: object) => void;
+
+enum RPCMethods {
+  PUBLISH        = "publish",
+  SUBSCRIBE      = 'subscribe',
+  UNSUBSCRIBE    = 'unsubscribe',
+  UNSUBSCRIBEALL = 'unsubscribeAll',
+  LIST           = 'list',
+  HISTORY        = 'history',
+  DISCONNECT     = 'disconnect',
+}
 
 export class Eventhub {
-  private websocketUri: string;
-  private authToken: string;
+  private _wsUrl: string;
+  private _socket: WebSocket;
 
-  private subscriptionCallbacks: Array<SubscriptionCallback>;
+  private _rpcResponseCounter: number;
+  // [ RPC ID, callback ]
+  private _rpcCallbackList: Array<[string, SubscriptionCallback]>;
+  private _subscriptionList: Array<string>;
 
-  /*
-  function subscribe(topics: string);
-  function subscribe(topics: Array<string>);
-*/
-
-  constructor (uri: string, token: string) {
-
+  constructor (url: string, token: string) {
+    this._rpcCallbackList = [];
+    this._rpcResponseCounter = 0;
+    this._subscriptionList = [];
+    this._wsUrl = `${url}/?auth=${token}`;
+    this._socket = undefined;
   }
 
-  public subscribe(topics: Array<string>|string, callback: SubscriptionCallback) : void {
-    if (typeof(topics) == 'string') {
-      console.log("Subscribe to single topic.");
-    } else {
-      console.log("Subscribe to multiple topics.");
+  /**
+   * Connects to the eventhub.
+   * @returns Promise with true on success or error string on fail.
+   */
+  public connect() : Promise<any> {
+    return new Promise(
+      (resolve, reject) => {
+        this._socket = new WebSocket(this._wsUrl);
+        this._socket.on('message', this.parseRPCResponse);
+
+        this._socket.on('open', function() {
+          resolve(true);
+        });
+
+        this._socket.on('error', function(err) {
+          reject(err);
+        })
+    });
+  }
+
+  /**
+   * Send a RPC request to the connected Eventhub.
+   */
+  private _sendRPCRequest(method: RPCMethods, params: Array<string>) : number {
+    this._rpcResponseCounter++;
+
+    const requestObject = {
+      id: this._rpcResponseCounter,
+      jsonrpc: "2.0",
+      method: method,
+      params: params,
     }
+
+    this._socket.send(JSON.stringify(requestObject));
+    return this._rpcResponseCounter;
   }
 
+  private parseRPCResponse(message: string, err: string ) : void {
+    console.log("Message:", message, "Error:", err);
+  }
+
+  /**
+   * Check if we are already subscribed to a topic.
+   * @param topic Topic.
+   */
+  public isSubscribed(topic: string) {
+    /*for (const subscribedTopic of this.subscriptionCallbacks) {
+      if (subscribedTopic[0] === topic) return true;
+    }*/
+
+    return false;
+  }
+
+  _/**
+   * Subscribe to one or more topic patterns.
+   * @param topics Array of topics or single topic to subscribe to.
+   * @param callback Callback to call when we receive a message the subscribed topic(s).
+   */
+  public subscribe(topics: Array<string>|string, callback: SubscriptionCallback) : void {
+    let topicList: Array<string>
+
+    if (typeof(topics) == 'string') {
+      topicList.push(topics);
+    } else {
+      topicList = topics;
+    }
+    
+    if (topicList.length < 1) {
+      throw Error("You must specify at least on topic to subscribe to.");
+    }
+
+    // First check if we are already subscribed to any of the given topics.
+    for (const topic of topicList) {
+      if (this.isSubscribed(topic)) {
+        throw new Error(`Already subscribed to ${topic}`);
+      }
+    }
+
+    // Do the actual subscription.
+    for (const topic of topicList) {
+      //this.subscriptionCallbacks.push([topic, callback]);
+    }
+    
+    this._sendRPCRequest(RPCMethods.SUBSCRIBE, topicList);    
+  }
+
+  /**
+   * Unsubscribe to one or more topic patterns.
+   * @param topics Array of topics or single topic to unsubscribe from.
+   */
   public unsubscribe(topics: Array<string>|string) : void {
 
   }
 
-
+  /**
+  * Unsubscribe from all topics.
+  * @returns Number of unsuscribed topics. 
+  */
   public unsubscribeAll() : number {
 
     return 0;
   }
 
-  public listSubscriptions() : Array<string> {
+  public publish(topic: string, message: string) {
 
-    return [];
+  }
+
+  public listSubscriptions() : Promise<Array<string>> {
+    return new Promise((resolve, reject) => {
+      
+    });
   }
 }
 
--- eventhub_test.ts
+++ eventhub_test.ts
@@ -1,3 +1,18 @@
 import { Eventhub } from "./Eventhub"
 
-let eventhub = new Eventhub("a", "b");
+let eventhub = new Eventhub("ws://127.0.0.1:8080", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIyMTQ2OTQ5MjMsInN1YiI6Im9sZS5za3Vkc3Zpa0BnbWFpbC5jb20iLCJ3cml0ZSI6WyJ0ZXN0MS8jIiwidGVzdDIvIyIsInRlc3QzLyMiXSwicmVhZCI6WyJ0ZXN0MS8jIiwidGVzdDIvIyIsInRlc3QzLyMiXSwiY3JlYXRlVG9rZW4iOlsidGVzdDEiLCJ0ZXN0MiIsInRlc3QzIl19.FSSecEiStcElHu0AqpmcIvfyMElwKZQUkiO5X_r0_3g");
+
+eventhub.connect().then(res=>{
+  eventhub.subscribe(["test1/test", "test2/test"], function (err, msg) {
+    if (err) {
+      console.log("Callback called err:", err);
+    }
+
+    console.log("Callback called msg:", msg);
+  });
+
+  console.log(res);
+}).catch(res=>{
+  console.log("Failed to connect:", res);
+});
+
