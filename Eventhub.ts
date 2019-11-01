a06a636c171eb67aac94d95bf7386450288154a6
Ole Fredrik Skudsvik <ole.fredrik.skudsvik@vg.no>
2019-11-01 02:49:28 +0100

    More work on the client

--- Eventhub.ts
+++ Eventhub.ts
@@ -1,43 +1,52 @@
 import WebSocket from 'ws';
 
-export declare type SubscriptionCallback = (err: object, message: object) => void;
+declare type RPCCallback = (err: string, message: string) => void;
+declare type SubscriptionCallback = (message: string) => void;
 
 enum RPCMethods {
-  PUBLISH        = "publish",
-  SUBSCRIBE      = 'subscribe',
-  UNSUBSCRIBE    = 'unsubscribe',
-  UNSUBSCRIBEALL = 'unsubscribeAll',
-  LIST           = 'list',
-  HISTORY        = 'history',
-  DISCONNECT     = 'disconnect',
+  PUBLISH         = "publish",
+  SUBSCRIBE       = 'subscribe',
+  UNSUBSCRIBE     = 'unsubscribe',
+  UNSUBSCRIBE_ALL = 'unsubscribeAll',
+  LIST            = 'list',
+  HISTORY         = 'history' // Not implemented yet.
+}
+
+class Subscription {
+  topic: string
+  rpcRequestId: number
+  callback: SubscriptionCallback
 }
 
 export class Eventhub {
   private _wsUrl: string;
   private _socket: WebSocket;
-
   private _rpcResponseCounter: number;
-  // [ RPC ID, callback ]
-  private _rpcCallbackList: Array<[string, SubscriptionCallback]>;
-  private _subscriptionList: Array<string>;
+  private _rpcCallbackList: Array<[number, RPCCallback]>;
+  private _subscriptionCallbackList: Array<Subscription>;
 
+  /**
+   * Constructor (new Eventhub).
+   * @param url Eventhub websocket url.
+   * @param token Authentication token.
+   */
   constructor (url: string, token: string) {
-    this._rpcCallbackList = [];
     this._rpcResponseCounter = 0;
-    this._subscriptionList = [];
+    this._rpcCallbackList = [];
+    this._subscriptionCallbackList = [];
     this._wsUrl = `${url}/?auth=${token}`;
     this._socket = undefined;
   }
 
   /**
    * Connects to the eventhub.
-   * @returns Promise with true on success or error string on fail.
+   * @return Promise with true on success or error string on fail.
    */
   public connect() : Promise<any> {
     return new Promise(
       (resolve, reject) => {
         this._socket = new WebSocket(this._wsUrl);
-        this._socket.on('message', this.parseRPCResponse);
+        this._socket.on('message', this._parseRPCResponse.bind(this));
 
         this._socket.on('open', function() {
           resolve(true);
@@ -51,8 +60,11 @@ export class Eventhub {
 
   /**
    * Send a RPC request to the connected Eventhub.
+   * @param method Which RPCMethod to call.
+   * @param params What parameters to include with the call.
+   * @return Promise with response or error.
    */
-  private _sendRPCRequest(method: RPCMethods, params: Array<string>) : number {
+  private _sendRPCRequest(method: RPCMethods, params: any) : Promise<any> {
     this._rpcResponseCounter++;
 
     const requestObject = {
@@ -62,12 +74,74 @@ export class Eventhub {
       params: params,
     }
 
-    this._socket.send(JSON.stringify(requestObject));
-    return this._rpcResponseCounter;
+    return new Promise((resolve, reject) => {
+      this._rpcCallbackList.push([this._rpcResponseCounter, function (err: string, resp: string) {
+        if (err != null) {
+          reject(err);
+        } else {
+          resolve(resp);
+        }
+      }.bind(this)]);
+
+      this._socket.send(JSON.stringify(requestObject));
+    });
   }
 
-  private parseRPCResponse(message: string, err: string ) : void {
-    console.log("Message:", message, "Error:", err);
+  /**
+   * Parse incoming websocket response and call correct handlers.
+   * @param response Response string.
+   */
+  private _parseRPCResponse(response: string) : void {
+    try {
+      const responseObj : Object = JSON.parse(response);
+
+      if(!responseObj.hasOwnProperty('id') || responseObj['id'] == 'null') {
+        console.log("Got RPC Response without ID:", response);
+        return;
+      }
+
+      // If this is a message publish then check if we are subscribed and call the corrrect callback.
+      if (responseObj.hasOwnProperty('result') && responseObj['result'].hasOwnProperty('message') && responseObj['result'].hasOwnProperty('topic')) {
+        for (let subscription of this._subscriptionCallbackList) {
+          if (subscription.rpcRequestId == responseObj['id']) {
+            subscription.callback(responseObj['result']);
+            return;
+          }
+        }
+      }
+
+      // This is not a publish response.
+      let rpcCallback = undefined;
+      for (let callback of  this._rpcCallbackList) {
+        if (callback[0] == responseObj['id']) {
+          rpcCallback = callback[1];
+          break;
+        }
+      }
+
+      // We have no callback for the response, do nothing.
+      if (rpcCallback === undefined) {
+        return;
+      }
+
+      // We have a callback for the response, call it.
+      if (responseObj.hasOwnProperty('error')) {
+        rpcCallback(responseObj['error'], null);
+      } else if (responseObj.hasOwnProperty('result')) {
+        rpcCallback(null, responseObj['result']);
+      }
+
+      // If it's not a subscription response, remove the callback once we got a response.
+      for (let i = 0; i < this._rpcCallbackList.length; i++) {
+        if (this._rpcCallbackList[i][0] == responseObj['id']) {
+          this._rpcCallbackList.splice(i, 1);
+        }
+      }
+
+    } catch (err) {
+      console.log("Failed to parse websocket response:", err);
+      return;
+    }
   }
 
   /**
@@ -75,10 +149,9 @@ export class Eventhub {
    * @param topic Topic.
    */
   public isSubscribed(topic: string) {
-    /*for (const subscribedTopic of this.subscriptionCallbacks) {
-      if (subscribedTopic[0] === topic) return true;
-    }*/
-
+    for (const subscribedTopic of this._subscriptionCallbackList) {
+      if (subscribedTopic.topic == topic) return true;
+    }
     return false;
   }
 
@@ -87,16 +160,17 @@ export class Eventhub {
    * @param topics Array of topics or single topic to subscribe to.
    * @param callback Callback to call when we receive a message the subscribed topic(s).
    */
-  public subscribe(topics: Array<string>|string, callback: SubscriptionCallback) : void {
-    let topicList: Array<string>
+  public subscribe(topics: Array<string>|string, callback: SubscriptionCallback) : Promise<any> {
+    let topicList: Array<string> = [];
 
     if (typeof(topics) == 'string') {
       topicList.push(topics);
     } else {
       topicList = topics;
     }
-    
+
     if (topicList.length < 1) {
+      // Todo: Return a error promise instead of throwing.
       throw Error("You must specify at least on topic to subscribe to.");
     }
 
@@ -109,10 +183,14 @@ export class Eventhub {
 
     // Do the actual subscription.
     for (const topic of topicList) {
-      //this.subscriptionCallbacks.push([topic, callback]);
+      this._subscriptionCallbackList.push({
+        topic: topic,
+        rpcRequestId: (this._rpcResponseCounter+1),
+        callback: callback
+      });
     }
-    
-    this._sendRPCRequest(RPCMethods.SUBSCRIBE, topicList);    
+
+    return this._sendRPCRequest(RPCMethods.SUBSCRIBE, topicList);
   }
 
   /**
@@ -120,26 +198,57 @@ export class Eventhub {
    * @param topics Array of topics or single topic to unsubscribe from.
    */
   public unsubscribe(topics: Array<string>|string) : void {
+    let topicList: Array<string> = [];
 
+    if (typeof(topics) == 'string') {
+      topicList.push(topics);
+    } else {
+      topicList = topics;
+    }
+
+    if (topicList.length > 0) {
+      for (let topic of topicList) {
+        for (let i = 0; i < this._subscriptionCallbackList.length; i++) {
+          if (topic == this._subscriptionCallbackList[i].topic) {
+            this._subscriptionCallbackList.splice(i, 1);
+          }
+        }
+      }
+
+      this._sendRPCRequest(RPCMethods.UNSUBSCRIBE, topicList);
+    }
   }
 
   /**
   * Unsubscribe from all topics.
-  * @returns Number of unsuscribed topics. 
+  * @returns Number of unsuscribed topics.
   */
   public unsubscribeAll() : number {
-
+    this._subscriptionCallbackList = [];
+    this._sendRPCRequest(RPCMethods.UNSUBSCRIBE_ALL, []);
     return 0;
   }
 
-  public publish(topic: string, message: string) {
+  /**
+   * Publish a message.
+   * @param topic Topic to publish to.
+   * @param message Message to publish.
+   */
+  public publish(topic: string, message: string) : Promise<any> {
+    const publishRequest = {
+      topic: topic,
+      message: message
+    }
 
+    return this._sendRPCRequest(RPCMethods.PUBLISH, publishRequest);
   }
 
-  public listSubscriptions() : Promise<Array<string>> {
-    return new Promise((resolve, reject) => {
-      
-    });
+  /**
+   * List all current subscriptions.
+   * @return Array containing current subscribed topics.
+   */
+  public listSubscriptions() : Promise<any> {
+    return this._sendRPCRequest(RPCMethods.LIST, []);
   }
 }
 
--- eventhub_test.ts
+++ eventhub_test.ts
@@ -3,15 +3,30 @@ import { Eventhub } from "./Eventhub"
 let eventhub = new Eventhub("ws://127.0.0.1:8080", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjIyMTQ2OTQ5MjMsInN1YiI6Im9sZS5za3Vkc3Zpa0BnbWFpbC5jb20iLCJ3cml0ZSI6WyJ0ZXN0MS8jIiwidGVzdDIvIyIsInRlc3QzLyMiXSwicmVhZCI6WyJ0ZXN0MS8jIiwidGVzdDIvIyIsInRlc3QzLyMiXSwiY3JlYXRlVG9rZW4iOlsidGVzdDEiLCJ0ZXN0MiIsInRlc3QzIl19.FSSecEiStcElHu0AqpmcIvfyMElwKZQUkiO5X_r0_3g");
 
 eventhub.connect().then(res=>{
-  eventhub.subscribe(["test1/test", "test2/test"], function (err, msg) {
-    if (err) {
-      console.log("Callback called err:", err);
-    }
-
+  eventhub.subscribe(["test1/#", "test2/nisse"], function (msg) {
     console.log("Callback called msg:", msg);
+  }).then(res => {
+    console.log("Subscribe success:", res)
+  }).catch(res=>{
+    console.log("Failed to subscribe:", res);
+  });;
+
+  eventhub.listSubscriptions().then(foo=>{
+    console.log("List:", foo);
   });
 
-  console.log(res);
+  eventhub.unsubscribe("test1/testt");
+  eventhub.subscribe("test2/foobar", function (msg) {});
+  eventhub.listSubscriptions().then(foo=>{
+    console.log("List:", foo);
+  });
+
+  setInterval(
+  function() {
+    eventhub.publish("test1/test", "FOOBAR");
+    eventhub.publish("test2/nisse", "BARFOO");
+  }, 1000);
+
 }).catch(res=>{
   console.log("Failed to connect:", res);
 });
